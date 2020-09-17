13f7e17ba43a9ed3178da14d1d40a332af936e9e
Jordan Tyus <jtyus@stetson.edu>
2020-09-17 17:56:32 -0400

    Community Hibernate Mapping/DAO

---
+++ src/main/java/com/revature/repositories/CommunityDAO.java
@@ -0,0 +1,60 @@
+package com.revature.repositories;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.Community;
+import com.revature.util.HibernateUtility;
+
+public class CommunityDAO implements ICommunityDAO{
+
+	@Override
+	public Set<Community> findAll() {
+		
+		Session s = HibernateUtility.getSession();
+		Transaction tx = s.beginTransaction();
+		
+		Set<Community> result = s.createQuery("FROM Community c", Community.class)
+				.getResultStream()
+				.collect(Collectors.toSet());
+		
+		tx.commit();
+		
+		return result;
+	}
+
+	@Override
+	public Community findById(int id) {
+		// TODO Auto-generated method stub
+		Session s = HibernateUtility.getSession();
+		return s.get(Community.class, id);
+	}
+
+	@Override
+	public Community findByName(String name) {
+		// TODO Auto-generated method stub
+		Session s = HibernateUtility.getSession();
+		return s.get(Community.class, name);
+	}
+
+	@Override
+	public boolean insertCommunity(Community c) {
+		// TODO Auto-generated method stub
+		Session s = HibernateUtility.getSession();
+		Transaction tx = s.beginTransaction();
+		
+		String id = (String) s.save(c);
+		
+		if(id != null && !id.equals("")) {
+			tx.commit();
+			return true;
+		}
+		
+		tx.rollback();
+		return false;
+	}
+
+}
