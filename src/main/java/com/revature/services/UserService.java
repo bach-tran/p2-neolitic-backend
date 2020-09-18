b890934e35b4f1108fc920e863b9745dcc85392d
bach-tran <bach_tran@outlook.com>
2020-09-17 21:26:24 -0500

    Reorganized UserService

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -83,7 +83,34 @@ public class UserService {
 			return null;
 		}
 	}
-	
+		
+	/* 
+	 * The following two methods are used to hash passwords with SHA-256. 
+	 * 
+	 * The first generates the password hash as a byte array
+	 * The second converts the password into a hex string
+	 */
+
+	public byte[] getSHA(String input) throws NoSuchAlgorithmException 
+    {  
+        MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
+    } 
+    
+    public String toHexString(byte[] hash) 
+    { 
+        BigInteger number = new BigInteger(1, hash);  
+  
+        StringBuilder hexString = new StringBuilder(number.toString(16));  
+  
+        while (hexString.length() < 32)  
+        {  
+            hexString.insert(0, '0');  
+        }  
+  
+        return hexString.toString();  
+    } 
+    
 //	public boolean createPost(byte[] image, String caption, Community community)
 //	{
 //		return false;
@@ -112,32 +139,5 @@ public class UserService {
 //	{
 //		return false;
 //	}
-	
-	/* 
-	 * The following two methods are used to hash passwords with SHA-256. 
-	 * 
-	 * The first generates the password hash as a byte array
-	 * The second converts the password into a hex string
-	 */
-
-	public byte[] getSHA(String input) throws NoSuchAlgorithmException 
-    {  
-        MessageDigest md = MessageDigest.getInstance("SHA-256"); 
-        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
-    } 
-    
-    public String toHexString(byte[] hash) 
-    { 
-        BigInteger number = new BigInteger(1, hash);  
-  
-        StringBuilder hexString = new StringBuilder(number.toString(16));  
-  
-        while (hexString.length() < 32)  
-        {  
-            hexString.insert(0, '0');  
-        }  
-  
-        return hexString.toString();  
-    } 
     
 }
