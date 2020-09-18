bccfcc2cfcbb9a34298850419f87001271ea46a3
Jordan Tyus <jtyus@stetson.edu>
2020-09-18 16:50:56 -0400

    Added Post Functionality

---
+++ src/main/java/com/revature/controllers/PostController.java
@@ -0,0 +1,48 @@
+package com.revature.controllers;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.revature.annotations.AuthorizedConsumer;
+import com.revature.dto.AddPostDTO;
+import com.revature.exceptions.PostException;
+import com.revature.models.Post;
+import com.revature.services.PostService;
+
+public class PostController {
+	
+	@Autowired
+	private PostService postService;
+	
+	private static Logger log = Logger.getLogger(PostController.class);
+	
+	@AuthorizedConsumer
+	@RequestMapping(value = "/post", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> getPostsInCommunity(@RequestBody AddPostDTO dto) throws PostException {
+		log.info("getPosts method invoked");
+		
+		
+		List<Post> posts = postService.getPosts(dto.getC());
+		
+		return ResponseEntity.ok(posts);
+	}
+	
+	@AuthorizedConsumer
+	@RequestMapping(value = "/post", method = RequestMethod.GET)
+	public ResponseEntity<Post> addPostToCommunity(@RequestBody AddPostDTO dto) throws PostException {
+		log.info("getPosts method invoked");
+		
+		
+		Post post = postService.addPost(dto.getImage(), dto.getCaption(), dto.getC(), dto.getAuthor().getId());
+		
+		return ResponseEntity.ok(post);
+	}
+	
+
+}
---
+++ src/main/java/com/revature/services/PostService.java
@@ -0,0 +1,50 @@
+package com.revature.services;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.revature.exceptions.PostException;
+import com.revature.models.Community;
+import com.revature.models.Post;
+import com.revature.repositories.IPostDAO;
+
+public class PostService {
+
+	@Autowired
+	private IPostDAO postDAO;
+	
+	private static Logger log = Logger.getLogger(PostService.class);
+	
+	public PostService() {
+		super();
+	}
+
+	public Post addPost(byte[] image,  String caption, Community community, int author) throws PostException {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		Post p = new Post(0, image, caption, community, author, timestamp);
+		
+		
+		Post insertedPost = postDAO.insertPost(p);
+		
+		if(insertedPost == null) {
+			log.error("Post was not successfully inserted");
+			throw new PostException("Post was not successfully inserted.");
+		}
+		
+		return insertedPost;
+	}
+	
+	public List<Post> getPosts(Community c) {
+		List<Post> posts = postDAO.findAllInCommunity(c);
+		
+		if (posts == null) {
+			posts = new ArrayList<Post>();
+		}
+		
+		return posts;
+	}
+}
