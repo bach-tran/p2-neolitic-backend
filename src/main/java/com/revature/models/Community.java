14a59be48194afc82d2180861ea37dbf08d3fa29
bach-tran <bach_tran@outlook.com>
2020-09-18 20:04:12 -0500

    Added many to one mappings from post to community and user. Disabled hibernate logging for TRACE and DEBUG levels. Modified DatabaseSetup class for testing

--- src/main/java/com/revature/models/Community.java
+++ src/main/java/com/revature/models/Community.java
@@ -4,10 +4,13 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -28,9 +31,10 @@ public class Community {
 	
 	@Column(nullable = false)
 	private String description;
-		
-	@OneToMany(mappedBy="community")
-	Set<Post> posts;
+	
+//	@JsonManagedReference
+//	@OneToMany(mappedBy="community")
+//	Set<Post> posts;
 
 	public Community(int id, String name, String description) {
 		super();
