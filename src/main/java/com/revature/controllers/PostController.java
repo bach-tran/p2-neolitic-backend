812c7f2be535ebf6fbb9bb37940200bc01f345e3
bach-tran <bach_tran@outlook.com>
2020-09-19 15:17:48 -0500

    Preliminary work on adding posts

--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -5,11 +5,15 @@ import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.revature.annotations.AuthorizedConsumer;
 import com.revature.dto.SendPostDTO;
@@ -34,22 +38,28 @@ public class PostController {
 		
 		Set<SendPostDTO> postsDto = new HashSet<>();
 		for (Post post : posts) {
+			post.getAuthor().setPassword("");
 			postsDto.add(new SendPostDTO(post.getId(), post.getCaption(), post.getAuthor(), post.getTimePosted()));
 		}
 		
 		return ResponseEntity.ok(postsDto);
 	}
 	
-//	@AuthorizedConsumer
-//	@RequestMapping(value = "/post", method = RequestMethod.GET)
-//	public ResponseEntity<Post> addPostToCommunity(@RequestBody AddPostDTO dto) throws PostException {
+	@AuthorizedConsumer
+	@RequestMapping(value = "/post", method = RequestMethod.POST)
+	public /*ResponseEntity<Post>*/ void addPostToCommunity(@RequestParam("communityId") int id, @RequestParam("caption") String caption, 
+			@RequestParam("file") MultipartFile file) throws PostException {
+		
 //		log.info("getPosts method invoked");
-//		
-//		
+		log.info(caption);
+		log.info(id);
+		String contentType = file.getContentType();
+		MediaType mt = MediaType.parseMediaType(contentType);
+		System.out.println(mt.getType() + mt.getSubtype());
 //		Post post = postService.addPost(dto.getImage(), dto.getCaption(), dto.getC(), dto.getAuthor());
-//		
+		
 //		return ResponseEntity.ok(post);
-//	}
+	}
 	
 
 }
