9c7c1582224fb75db796f74e5b5194aab2bdf11f
bach-tran <bach_tran@outlook.com>
2020-09-20 15:18:17 -0500

    Refactor GlobalExceptionHandler to handle multiple exceptions in one method

--- src/main/java/com/revature/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/revature/controllers/GlobalExceptionHandler.java
@@ -13,8 +13,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.revature.exceptions.AddPostException;
 import com.revature.exceptions.CommunityException;
+import com.revature.exceptions.GetImageException;
 import com.revature.exceptions.LoginException;
+import com.revature.exceptions.PostException;
 import com.revature.exceptions.RegistrationException;
 import com.revature.util.HibernateUtility;
 
@@ -25,29 +28,8 @@ public class GlobalExceptionHandler {
 	
 	// ====================================================================================================================
 	// Exception handlers
-//	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Unable to login using provided credentials")
-	@ExceptionHandler(LoginException.class)
-	public void loginException(Exception ex) throws IOException {
-		HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
-		if (response != null) {
-			response.sendError(400, ex.getMessage());
-		}
-		
-		log.error(ex.getMessage());
-	}
 	
-//	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Unable to register using provided credentials.")
-	@ExceptionHandler(RegistrationException.class)
-	public void registrationException(Exception ex) throws IOException {
-		HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
-		if (response != null) {
-			response.sendError(400, ex.getMessage());
-		}
-		
-		log.error(ex.getMessage());
-	}
-	
-	@ExceptionHandler(CommunityException.class)
+	@ExceptionHandler({CommunityException.class, RegistrationException.class, LoginException.class, PostException.class, AddPostException.class, GetImageException.class})
 	public void communityException(Exception ex) throws IOException {
 		HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
 		if (response != null) {
